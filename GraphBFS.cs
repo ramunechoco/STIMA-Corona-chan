7db2b2372ffacc53801b193d9b2a58670ee88e1a
Neptune3X <52845093+Neptune3X@users.noreply.github.com>
2020-03-10 14:25:01 +0700

    Adding time_f ke Vertex sama perbaikan rumus

--- GraphBFS.cs
+++ GraphBFS.cs
@@ -1,139 +1,120 @@
 using System;
 using System.Collections.Generic;
+using System.Ling;
+using System.Text;
+using System.Threading.Tasks;
+using Region;
 
-namespace bfs
-{
-    class Vertex
-    {
-        String name;
-        Tonari adjlist;
-        private int population;
-
-        Vertex(String name, Tonari adjlist, int population)
-        {
+namespace bfs{
+    class Vertex{
+        String name;//City
+        Tonari adjlist;//Tr...
+        private int population;//Population 
+        private int time_f;//Time
+        
+        Vertex(String name,Tonari adjlist,int population,int time_f){
             this.name = name;
             this.adjlist = adjlist;
             this.population = population;
+            this.time_f = time_f;
         }
-        public string GetName()
-        {
-            return name;
+
+        Vertex(String name,Tonari adjlist){
+            this.name = name;
+            this.adjlist = adjlist;
+            this.population = 0;
+            this.time_f = 0;
         }
-        public Tonari GetAdjlist()
-        {
-            return adjlist;
+
+        public int time_if(int t){
+            return t - this.time_f;
+        }
+
+        public void set_time_f(int t){
+            this.time_f = t;
+        }
+
+        public int get_time_f(){
+            return this.time_f;
+        }
+
+        public void set_population(int pop){
+            this.population = pop;
+        }
+
+        public int get_population(){
+            return this.population;
         }
     }
 
-    class Tonari
-    {
-        public int vertexNum; //-->Tr(...,...)
+    class Tonari{
+        public double transmission; //Tr(A,...)
         public Tonari next;
-
-        public Tonari(int vnum, Tonari k)
-        {
-            this.vertexNum = vnum;
+        
+        public Tonari(int vnum,Tonari k){
+            this.transission = vnum;
             next = k;
         }
 
-        public double[] infectedPeople(Vertex[] cityA, int time)
-        {//-->I(P(A),T(A))
-            double time1 = Convert.ToDouble(time);
-            double infected = new double[cityA.length];
-
-            for (int v = 0; v < cityA.length; v++)
-            {
-                infected[v] = cityA[v].population * time1 / 20;
-            }
+        public double infectedPeople(Vertex cityA){//-->I(P(A),T(A))
+            double time1 = (double) cityA.time_f;
+            double infected = cityA.population * time1 / 20;
             return infected;
         }
 
-        public double infectedF(Vertex[] cityA, int time)
-        {
-            double time1 = Convert.ToDouble(time);
+        public double infectedF(Vertex cityA){
+            double time1 = (double) cityA.time_f;
+            
+            double upperlevel;
+            double lowerlevel;
 
-            double upperlevel = new double[cityA.length];
-            double lowerlevel = new double[cityA.length];
+                upperlevel = cityA.population;
+                lowerlevel = 1 + (cityA.population - 1) * Math.Exp(time1 * (-1) / 4);
 
-            for (int v = 0; v < cityA.length; v++)
-            {
-                upperlevel[v] = cityA[v].population;
-                lowerlevel[v] = 1 + (cityA[v].population - 1) * Math.Exp(time1 * (-1) / 4);
-            }
-
-            double infectedF = new double[cityA.length];
-
-            for (int v = 0; v < cityA.length; v++)
-            {
-                infectedF[v] = upperlevel[v] / lowerlevel[v];
-            }
+                double infectedF = upperlevel/lowerlevel;
 
             return infectedF;
         }
 
-        public Vertex[] transferRate(Vertex[] cityA, int time, Tonari k)
-        {//S(A,B)
-            Vertex[] transferRate = new Vertex[cityA.length];
-            bool finish;
-            finish = false;
-            while (!finish)
-            {
-                for (k = cityA[v].adjlist; k != null; k = k.next)
-                {
-                    transferRate[v].adjlist = infectedPeople(cityA, time) * k.vertexNum;
-                    if (k != null)
-                    {
-                        continue;
-                    }
-                    else if (k == null && v < cityA.length)
-                    {
-                        v++;
-                    }
-                    else
-                    {
-                        finish = true;
-                    }
-                }
+        public bool transferRate(Vertex cityA){//S(A,B)
+            bool infected = false;
+            double transferRate = cityA.adjlist.transission * infectedPeople(cityA);
+
+            if(transferRate < 1){
+                infected = true;
+            }
+            else{
+                infected = false;
             }
 
-            return transferRate;
-        }
+            return infected;
     }
 
-
-    public class graph
-    {
+    public class graph{
         Vertex[] adjlist;
 
-        public void bfs()
-        {
-            Queue<Int32> queue = new Queue<int>();
-            bool[] visited = new bool[adjlist.Length];
-            for (int v = 0; v < visited.Length; v++)
-            {
-                if (!visited[v])
-                {
-                    bfs(v, visited, queue);
+        public void bfs(){
+            Queue<Int32> queue = new Queue<Int>();
+            bool[] visited = new bool[adjlist.length];
+            for (int v = 0; v < visited.length; v++){
+                if(!visited[v]){
+                    bfs(v,visited,queue);
                 }
             }
         }
 
-        private void bfs(int start, bool[] visited, Queue<Int32> queue)
-        {
-            int time;
+        private void bfs(int start,bool[] visited,Queue<Int32> queue){
             visited[start] = true;
-            Console.WriteLine("Visiting " + adjlist[start].GetName());
+            Console.Writeln("Visiting " + adjlist[start].name);
             queue.Enqueue(start);
 
-            while (queue.Count != 0)
-            {
+            while(!queue.IsEmpty()){
                 int v = queue.Dequeue();
-                for (Tonari k = adjlist[v].GetAdjlist(); k != null; k = k.next)
-                {
-                    int vnum = k.vertexNum;
-                    if (!visited[vnum] && transferRate(adjlist, time, k))
-                    {
-                        Console.WriteLine("Visiting " + adjlist[vnum].name);
+                for(Tonari k = adjlist[v].adjlist; k != null; k = k.next){
+                    int vnum = k.transmission;
+
+                    if(!visited[vnum] && transferRate(adjlist)){
+                        Console.Writeln("Visiting " + adjlist[vnum].name);
                         visited[vnum] = true;
                         queue.Enqueue(vnum);
                     }
@@ -141,4 +122,5 @@ namespace bfs
             }
         }
     }
+}
 }
